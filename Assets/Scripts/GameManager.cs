5af9890e8ab3c46190a4f894fbed7cd12e333f13
Setseth <Sethvpairish@gmail.com>
2022-05-17 17:01:48 -0700

    LessonD

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -7,23 +7,28 @@ using UnityEngine.UI;
 
 public class GameManager : MonoBehaviour
 {
-    private const float spawnRate = 2.0f;
+    private float spawnRate = 2.0f;
     public List<GameObject> prefabs;
 
     public TextMeshProUGUI scoreText;
     public TextMeshProUGUI gameOverText;
     public Button RestartButton;
+    public GameObject titleScreen;
 
     private int score = 0;
-    public bool gameActive = true;
+    public bool gameActive = false;
 
-    // Start is called before the first frame update
-    void Start()
+    public void StartGame(int diff)
     {
+        gameActive = true;
+        score = 0;
+        spawnRate = spawnRate / diff;
+        Debug.Log("game spawn rate = " + spawnRate);
         StartCoroutine(SpawnTarget());
         UpdateScore(0);
+        titleScreen.gameObject.SetActive(false);
     }
-
+    
     public void GameOver()
     {
         gameActive = false;
